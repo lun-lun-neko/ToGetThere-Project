5f8cf7d101624b2d11bdbe4986c6b4ac13e53311
lun-lun-neko <eyo8163@g.kmou.ac.kr>
2026-04-29 01:23:24 +0900

    jhgan/ko-sroberta-multitask 한글 모델 적용

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+model = SentenceTransformer("jhgan/ko-sroberta-multitask")
 
 testSentence = ["어제부터 감기 기운이 있는데 병원을 가봐야 하나 고민돼",
                 "내일 이비인후과를 갈까 하는데 혹시 같이 내원할 사람 있나..?",
                 "내일 이비인후과에 같이 갈 사람 있나요?",
-                "오늘 같이 병원 갈 사람!"]
+                "오늘 같이 병원 갈 사람!",
+                "롤 팟 1명 구함"]
 
 
 emb = model.encode(testSentence)
@@ -16,12 +17,36 @@ print(emb.shape)
 similarities = model.similarity(emb, emb)
 print(similarities)
 
+print("exist : \n 어제부터 감기 기운이 있는데 병원을 가봐야 하나 고민돼 \n",
+        "내일 이비인후과를 갈까 하는데 혹시 같이 내원할 사람 있나..? \n",
+        "내일 이비인후과에 같이 갈 사람 있나요? \n",
+        "오늘 같이 병원 갈 사람! \n",
+        "롤 팟 1명 구함")
+
 newSentence = "오늘 같이 헬스장 갈 사람 있을까요"
 
 embNew = model.encode([newSentence])
 
 simi = model.similarity(embNew, emb)
+print("new : 오늘 같이 헬스장 갈 사람 있을까요")
 print(simi)
+
+newSentence = "칼바람 하실 분"
+
+embNew = model.encode([newSentence])
+
+simi = model.similarity(embNew, emb)
+print("new : 칼바람 하실 분")
+print(simi)
+
+newSentence = "병원 갈 사람"
+
+embNew = model.encode([newSentence])
+
+simi = model.similarity(embNew, emb)
+print("new : 병원 갈 사람")
+print(simi)
+
 
 # 영어 문장 테스트
 """
